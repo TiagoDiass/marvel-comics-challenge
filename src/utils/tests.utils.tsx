e91140499812164d4be0057582b732548c43eb09
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-21 14:10:10 -0300

    chore: add comments on test utils

--- src/utils/tests.utils.tsx
+++ src/utils/tests.utils.tsx
@@ -3,9 +3,15 @@ import { render, RenderResult } from '@testing-library/react';
 import { IComicsListContext } from 'contexts/ComicsList.context';
 import theme from 'styles/theme';
 
+/**
+ * @helper renderiza o componente com o tema utilizado pelo ThemeProvider do styled-components
+ */
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 
+/**
+ * @helper factory que retorna um mock do valor do contexto de ComicsListContext
+ */
 export const mockComicsListContextValue = (): IComicsListContext => ({
   addComic: jest.fn(),
   removeComic: jest.fn(),
