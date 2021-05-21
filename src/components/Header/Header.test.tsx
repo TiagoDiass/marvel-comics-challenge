1a8f80955a09325411c737de74bc38ca4e67b272
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-21 14:06:49 -0300

    test: ensure Header shows that there are more than one comic in the list correctly

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -15,6 +15,9 @@ const makeSut = (comicsListContextValue: IComicsListContext) => {
 };
 
 describe('Header component', () => {
+  // testes para validar que a label de quantidade de comics aparece corretamente, já que ela pode aparecer como:
+  // "Nenhum quadrinho selecionado", "1 quadrinho", "X quadrinhos"
+
   it('should show that there are no comics in the list if total comics amount is 0', () => {
     makeSut({
       ...mockComicsListContextValue(),
@@ -31,10 +34,20 @@ describe('Header component', () => {
   it('should show that there are only one comic in the list if total comics amount is 1', () => {
     makeSut({
       ...mockComicsListContextValue(),
-      getTotalComics: () => 1, // mockando o getTotalComics para retornar 0, e assim o componente renderizar a label esperada
+      getTotalComics: () => 1, // mockando o getTotalComics para retornar 1, e assim o componente renderizar a label esperada
     });
 
     const amountLabel = screen.getByTestId('amount-label');
     expect(amountLabel).toHaveTextContent('1 quadrinho');
+  });
+
+  it('should show that there are more than one comic in the list if total comics amount is greater than 1', () => {
+    makeSut({
+      ...mockComicsListContextValue(),
+      getTotalComics: () => 4, // mockando o getTotalComics para retornar 4, e assim o componente renderizar a label esperada
+    });
+
+    const amountLabel = screen.getByTestId('amount-label');
+    expect(amountLabel).toHaveTextContent('4 quadrinhos');
   });
 });
