c070da9e70ed9f8ed8792d096a12554785949656
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-21 11:14:49 -0300

    feat: add Header's responsive styles

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,13 +1,22 @@
-import styled, { css, keyframes } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const HeaderContainer = styled.header`
-  height: 6.5rem;
+  min-height: 6.5rem;
   background-color: ${({ theme }) => theme.colors.primaryDarker};
 
   display: flex;
   align-items: center;
   padding: 2rem 8rem;
   border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+
+  @media (max-width: 960px) {
+    padding: 2rem 4rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 2rem;
+    flex-direction: column;
+  }
 `;
 
 export const Logo = styled.h1`
@@ -29,7 +38,16 @@ export const Slogan = styled.p`
     margin-left: 2rem;
     padding: 0.25rem 0px 0.25rem 2rem;
     border-left: 1px solid ${theme.colors.whiteDarker};
+
+    @media (max-width: 768px) {
+      margin: 0;
+      padding: 0;
+      margin-top: 0.6rem;
+      padding-top: 0.5rem;
+      border-left: none;
+      border-top: 1px solid ${theme.colors.whiteDarker};
+
+      text-align: center;
+    }
   `};
 `;
-
-
