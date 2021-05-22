e42d999216bf314b6b4874e643454714bf10b55d
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-22 15:28:09 -0300

    refactor: add global styles to buttons

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -26,6 +26,26 @@ const globalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.fonts.default};
   }
 
+  button {
+    font-family: ${({ theme }) => theme.fonts.default};
+    text-transform: uppercase;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    border: none;
+    outline: none;
+    border-radius: 4px;
+
+    cursor: pointer;
+
+    transition: all 0.2s ease;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.primaryDarker};
+    }
+  }
+
   .react-modal-overlay {
     background-color: rgba(0, 0, 0, 0.5);
 
