cf76d9a74acf5255c4a148078ecd324158cca554
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-22 13:01:25 -0300

    test: ensure convertAPIComicToComic sets a default description the API returned a falsy one

--- src/utils/convertAPIComicToComic/test.ts
+++ src/utils/convertAPIComicToComic/test.ts
@@ -3,7 +3,7 @@ import convertAPIComicToComic from '.';
 
 describe('ConvertAPIComicToComic util', () => {
   it('should convert an APIComic to a Comic correctly', () => {
-    const APIComic = mockApiComic();
+    const APIComic = mockApiComic({});
 
     const convertedComic = convertAPIComicToComic(APIComic);
 
@@ -11,6 +11,23 @@ describe('ConvertAPIComicToComic util', () => {
       id: APIComic.id,
       title: APIComic.title,
       description: APIComic.description,
+      thumbnailUrl: `${APIComic.thumbnail.path}/portrait_uncanny.${APIComic.thumbnail.extension}`,
+      totalPageCount: APIComic.pageCount,
+      creators: APIComic.creators.items.map(creator => creator.name),
+    });
+  });
+
+  it('should set description to a default value if the description from API is falsy', () => {
+    const APIComic = mockApiComic({});
+
+    APIComic.description = null;
+
+    const convertedComic = convertAPIComicToComic(APIComic);
+
+    expect(convertedComic).toEqual({
+      id: APIComic.id,
+      title: APIComic.title,
+      description: 'Sem descrição na base de dados',
       thumbnailUrl: `${APIComic.thumbnail.path}/portrait_uncanny.${APIComic.thumbnail.extension}`,
       totalPageCount: APIComic.pageCount,
       creators: APIComic.creators.items.map(creator => creator.name),
