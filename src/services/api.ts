ea07e2378a6dface0803b4200112c2eff8685215
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-22 14:20:23 -0300

    chore: add @service comment to standardize - API service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/**
+ * @service instância do axios configurada com BASE_URL e params padrão entre todas requests
+ */
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_MARVEL_API_URL,
   params: {
