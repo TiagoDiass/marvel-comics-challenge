6d2f357bb7ff7e382539060c478e2a2045fab381
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-22 15:47:35 -0300

    feat: add empty comics list text

--- src/pages/comics-list.tsx
+++ src/pages/comics-list.tsx
@@ -1,20 +1,14 @@
 import Head from 'next/head';
 import { BiMailSend } from 'react-icons/bi';
 import { MdDelete } from 'react-icons/md';
+import { useComicsListContext } from 'contexts/ComicsList.context';
 import * as S from 'styles/pages/comics-list.styles';
-import convertAPIComicToComic from 'utils/convertAPIComicToComic';
-import mock from '../../mock.json';
 
 /**
  * @page Página ComicsList
  */
 export default function ComicsList() {
-  const comicsList = [
-    convertAPIComicToComic(mock.results[0]),
-    convertAPIComicToComic(mock.results[1]),
-    convertAPIComicToComic(mock.results[2]),
-    convertAPIComicToComic(mock.results[3]),
-  ];
+  const { comicsList } = useComicsListContext();
 
   return (
     <>
@@ -26,57 +20,65 @@ export default function ComicsList() {
         <h1>Sua lista de quadrinhos</h1>
 
         <S.ContentContainer>
-          <S.ComicsTable>
-            <thead>
-              <tr>
-                <th>Capa</th>
-                <th>Título</th>
-                <th>ID</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
+          {comicsList.length ? (
+            <>
+              <S.ComicsTable>
+                <thead>
+                  <tr>
+                    <th>Capa</th>
+                    <th>Título</th>
+                    <th>ID</th>
+                    <th>Ações</th>
+                  </tr>
+                </thead>
 
-            <tbody>
-              {comicsList.map(comic => (
-                <tr key={comic.id}>
-                  <td>
-                    <figure>
-                      <img src={comic.thumbnailUrl} alt={comic.title} />
-                    </figure>
-                  </td>
+                <tbody>
+                  {comicsList.map(comic => (
+                    <tr key={comic.id}>
+                      <td>
+                        <figure>
+                          <img src={comic.thumbnailUrl} alt={comic.title} />
+                        </figure>
+                      </td>
 
-                  <td>
-                    <strong>{comic.title}</strong>
-                  </td>
+                      <td>
+                        <strong>{comic.title}</strong>
+                      </td>
 
-                  <td>
-                    <span>{comic.id}</span>
-                  </td>
+                      <td>
+                        <span>{comic.id}</span>
+                      </td>
 
-                  <td>
-                    <button>
-                      Remover da Lista <MdDelete size={16} />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </S.ComicsTable>
+                      <td>
+                        <button>
+                          Remover da Lista <MdDelete size={16} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </S.ComicsTable>
 
-          <S.Footer>
-            <div className='input-wrapper'>
-              <label htmlFor='email'>E-mail</label>
-              <input
-                id='email'
-                type='text'
-                placeholder='Digite um e-mail para enviar a lista de quadrinhos...'
-              />
-            </div>
+              <S.Footer>
+                <div className='input-wrapper'>
+                  <label htmlFor='email'>E-mail</label>
+                  <input
+                    id='email'
+                    type='text'
+                    placeholder='Digite um e-mail para enviar a lista de quadrinhos...'
+                  />
+                </div>
 
-            <button>
-              Enviar <BiMailSend size={20} />
-            </button>
-          </S.Footer>
+                <button>
+                  Enviar <BiMailSend size={20} />
+                </button>
+              </S.Footer>
+            </>
+          ) : (
+            <h2 style={{ textAlign: 'center' }}>
+              Ops... <br /> Parece que você ainda não adicionou nenhum quadrinho
+            </h2>
+          )}
         </S.ContentContainer>
       </S.ComicsListPageContainer>
     </>
