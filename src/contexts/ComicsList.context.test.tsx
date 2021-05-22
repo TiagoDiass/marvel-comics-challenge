c18c9630c369bf3923a1f79e5a7640507d6aa5f3
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-22 13:38:04 -0300

    test: ensure ComicsListContext adds a comic correctly

--- src/contexts/ComicsList.context.test.tsx
+++ src/contexts/ComicsList.context.test.tsx
@@ -18,4 +18,78 @@ describe('ComicsListContext context', () => {
     const amountOfComicsElement = screen.getByTestId('amount');
     expect(amountOfComicsElement).toHaveTextContent('amount of comics: 0');
   });
+
+  it('should add a comic correctly', () => {
+    const comicToBeAdded = generateComic({});
+
+    render(
+      <ComicsListContextProvider>
+        <ComicsListContext.Consumer>
+          {contextValue => (
+            <>
+              <button onClick={() => contextValue.addComic(comicToBeAdded)}>Adicionar comic</button>
+              <span data-testid='amount'>amount of comics: {contextValue.comicsList.length}</span>
+              <ul>
+                {contextValue.comicsList.map(comic => (
+                  <li key={comic.id}>{comic.id}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </ComicsListContext.Consumer>
+      </ComicsListContextProvider>
+    );
+
+    const getComicsListAmountElement = () => screen.getByTestId('amount');
+    const getComicsListElement = () => screen.getByRole('list');
+
+    expect(getComicsListAmountElement()).toHaveTextContent('amount of comics: 0');
+    expect(getComicsListElement().children).toHaveLength(0);
+
+    const addComicButton = screen.getByRole('button', { name: /adicionar comic/i });
+
+    userEvent.click(addComicButton);
+
+    expect(getComicsListAmountElement()).toHaveTextContent('amount of comics: 1');
+    expect(getComicsListElement().children).toHaveLength(1);
+  });
+
+  it('should not add a comic if it is already in the list', () => {
+    const comicToBeAdded = generateComic({});
+
+    render(
+      <ComicsListContextProvider>
+        <ComicsListContext.Consumer>
+          {contextValue => (
+            <>
+              <button onClick={() => contextValue.addComic(comicToBeAdded)}>Adicionar comic</button>
+              <span data-testid='amount'>amount of comics: {contextValue.comicsList.length}</span>
+              <ul>
+                {contextValue.comicsList.map(comic => (
+                  <li key={comic.id}>{comic.id}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </ComicsListContext.Consumer>
+      </ComicsListContextProvider>
+    );
+
+    const getComicsListAmountElement = () => screen.getByTestId('amount');
+    const getComicsListElement = () => screen.getByRole('list');
+    const addComicButton = screen.getByRole('button', { name: /adicionar comic/i });
+
+    expect(getComicsListAmountElement()).toHaveTextContent('amount of comics: 0');
+    expect(getComicsListElement().children).toHaveLength(0);
+
+    userEvent.click(addComicButton); // adicionando o comic
+
+    expect(getComicsListAmountElement()).toHaveTextContent('amount of comics: 1');
+    expect(getComicsListElement().children).toHaveLength(1);
+
+    userEvent.click(addComicButton); // adicionando o comic denovo, como é o mesmo comic, o tamanho ainda deve ser 1
+
+    expect(getComicsListAmountElement()).toHaveTextContent('amount of comics: 1');
+    expect(getComicsListElement().children).toHaveLength(1);
+  });
 });
